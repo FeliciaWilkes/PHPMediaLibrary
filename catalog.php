fe4e124ffda958e1253dfd52a005ba664db589c2
Felicia Wilkes <feliciankc@gmail.com>
2021-01-18 03:45:17 -0600

    Associative Array Data

--- catalog.php
+++ catalog.php
@@ -1,11 +1,6 @@
 <?php 
 include("./includes/header.php"); 
-
-$catalog = array ();
-$catalog [101] = "Design Patterns";
-$catalog [201] = "Forrest Gump";
-$catalog [301] = "Beethoven";
-$catalog [102] = "Clean Code";
+include("./includes/data.php");
 
 $pageTitle = "Full Catalog";
 $section = null;
